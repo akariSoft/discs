2d14f8248c06a25a97b337d4a7aa5336288246e3
Emmanuel Valle <emmakun@rockngeek.net>
2016-01-30 22:12:41 +0000

    Fixing cover processing and fininshing basic admin panel

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,28 +3,32 @@ class Item < ActiveRecord::Base
   has_and_belongs_to_many :languages, -> { uniq }
 
   dragonfly_accessor :front do
-    after_assign do |img_front|
-      img_front.convert!(%q[
+    after_assign do |img|
+      img.convert!(%q[
                        -strip -filter Triangle -define filter:support=2
                        -thumbnail 'x1081' -gravity east -crop '764x1081+0+0' -unsharp 0.25x0.25+8+0.065
                        -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off
                        -define png:compression-filter=5 -define png:compression-level=9
                        -define png:compression-strategy=1 -define png:exclude-chunk=all
                        -interlace none -colorspace sRGB -units PixelsPerInch -density 72
-                     ], 'format' => 'jpg')
+                     ], 'format' => 'jpg') unless img.width == 764 &&
+                                                  img.height == 1081 &&
+                                                  img.format == 'jpeg'
     end
   end
   
   dragonfly_accessor :back do
-    after_assign do |img_back|
-      img_back.convert!(%q[
+    after_assign do |img|
+      img.convert!(%q[
                        -strip -filter Triangle -define filter:support=2
-                       -thumbnail 'x1081' -gravity east -crop '764x1081+0+0' -unsharp 0.25x0.25+8+0.065
+                       -thumbnail 'x1081' -crop '764x1081+0+0' -unsharp 0.25x0.25+8+0.065
                        -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off
                        -define png:compression-filter=5 -define png:compression-level=9
                        -define png:compression-strategy=1 -define png:exclude-chunk=all
                        -interlace none -colorspace sRGB -units PixelsPerInch -density 72
-                     ], 'format' => 'jpg')
+                     ], 'format' => 'jpg') unless img.width == 764 &&
+                                                  img.height == 1081 &&
+                                                  img.format == 'jpeg'
     end
   end
 end
