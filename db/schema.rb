cada347034fa477a8d12dbba197f6c499577039c
Emmanuel Valle <emmakun@rockngeek.net>
2016-02-29 01:09:56 +0000

    Remove Clearance migration (Users table)

--- db/schema.rb
+++ db/schema.rb
@@ -72,16 +72,4 @@ ActiveRecord::Schema.define(version: 20160227203952) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
-    t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email"
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
-
 end
